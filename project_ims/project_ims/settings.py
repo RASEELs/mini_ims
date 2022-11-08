7b6a41bb8ceba3695bafe4591c1d6ed170454dc1
RASEELs <0raseel0@gmail.com>
2022-11-08 08:58:06 +0545

    db cofigured & mysql installed

--- project_ims/project_ims/settings.py
+++ project_ims/project_ims/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_ims',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +75,20 @@ WSGI_APPLICATION = 'project_ims.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'mini_ims',
+       'HOST': 'localhost',
+       'USER': 'root',
+       'PASSWORD':'',
+       'PORT': '3306'
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
