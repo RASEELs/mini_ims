44f5251f1b6196a1be461c2dd08ddd599fec5803
RASEELs <0raseel0@gmail.com>
2022-11-13 22:00:50 +0545

    create, index, show & edit page created with view & urls

--- project_ims/app_ims/urls.py
+++ project_ims/app_ims/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('items/', views.item_index, name="items.index"),
-    
+    path('items/create/', views.item_create, name="items.create"), 
+    path('items/edit/', views.item_edit, name="items.edit"),
+    path('items/show/', views.item_show, name="items.show"),
 ]
